34553ea95126e62952c3cfe679026d1e66fed7fb
MdShohan81 <mdshohan6563@gmail.com>
2022-05-06 10:59:19 +0600

    load data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +9,35 @@ const port = process.env.PORT || 5000;
 //use middleware
 app.use(cors());
 app.use(express.json());
+
+// UserName: carspot
+//password: PlJVaixSsopj5DjX
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pwbpg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('carspot').collection('product');
+
+        app.get('/product', async (req, res) =>{
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+            console.log(products)
+        });
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
 
 
 
