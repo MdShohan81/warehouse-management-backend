ab18b4b0bb7780d438eeaeb08e3d68eb920d5f27
MdShohan81 <mdshohan6563@gmail.com>
2022-05-06 11:24:55 +0600

    get single item by dynamic id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectID } = require('bson');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -30,6 +31,13 @@ async function run(){
             res.send(products);
             console.log(products)
         });
+
+        app.get('/product/:id', async (req, res) =>{
+            const id = req.params.id;
+            const query ={_id: ObjectID(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
 
     }
     finally{
