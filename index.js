d80006543f2fb3e91cc4794c44338952ef81e427
MdShohan81 <mdshohan6563@gmail.com>
2022-05-08 18:49:20 +0600

    added order delete api

--- index.js
+++ index.js
@@ -99,6 +99,13 @@ async function run(){
             const result = await productCollection.deleteOne(query);
             res.send(result);
         });
+
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+        })
         //Order Collection API
         app.get('/order', verifyJWT, async(req, res) => {
             const decodedEmail = req.decoded.email;
