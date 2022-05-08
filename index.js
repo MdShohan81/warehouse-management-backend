b61140a23eb817222b35c895d70076ce2e79025d
MdShohan81 <mdshohan6563@gmail.com>
2022-05-08 08:36:31 +0600

    added orders api

--- index.js
+++ index.js
@@ -73,6 +73,13 @@ async function run(){
             res.send(result);
         });
         //Order Collection API
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
